6e1c9272b0ebdb42fe8dfb331ea67dc59f8a8cfc
GalinaSherbak84 <galinasherbak@gmail.com>
2021-05-06 19:50:36 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      <img src="@/assets/logo.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+@import'@/styles/vars';
+    header{
+        padding:10px;
+        background: $mycolorLight;
+        img{
+            width: 30px;
+        }
+    }
+</style>
